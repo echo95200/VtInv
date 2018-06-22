3723ceaa82284394ed282a9fc3bf0165a3ad403f
echo95200 <jiaoqiangxu@gmail.com>
2018-06-22 15:11:19 +0200

    need to add mockup

--- vtinv.h
+++ vtinv.h
@@ -2,12 +2,18 @@
 #define VTINV_H
 
 #include "vtinv_global.h"
+#include <QWidget>
 
-class VTINVSHARED_EXPORT VtInv
+class VTINVSHARED_EXPORT VtInv : public QWidget
 {
+    Q_OBJECT
 
 public:
-    VtInv();
+    explicit VtInv(QWidget *parent = 0);
+
+private slots:
+//    void previewClicked();
+//    void designClicked();
 };
 
 #endif // VTINV_H
